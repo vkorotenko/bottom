500173dc9e3df08ec27fac498af393fea4dd08da
Vladimir N. Korotenko <koroten@ya.ru>
2019-04-16 21:27:51 +0300

    Add project files.

---
+++ Bottom/Bottom/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Bottom.Models;
+
+namespace Bottom.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
